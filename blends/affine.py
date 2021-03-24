14316ae88e53741b238f9c047c792a62b1b906c3
Kevin Nguyen <kevin3.nguyen@UTSouthwestern.edu>
2021-03-24 09:22:31 -0500

    Added documentation to affine augmenter module

--- blends/affine.py
+++ blends/affine.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-TODO: add documentaiton
+Classes for performing a simple augmentation of 3D/4D images via random affine transformations. 
 ______
 10/22/19
 """
@@ -25,6 +25,26 @@ class AffineAugmenter:
                  si_rot_range=np.arange(-20, 20, 1),
                  scale_range=np.arange(0.9, 1.1, 0.05),
                  seed=None):
+        """Augmentation through randomly generated affine transformations.
+
+        Initialize with desired transformation ranges, then call .run(). Input
+        images must be organized in a BIDS-style structure.
+
+        Requires FSL to be installed and on the system PATH.
+
+        Args:
+            input_files (list): list of original images with BIDS-compliant filename and directory structure
+            output_dir (str): path to output location
+            n_augment (int): number of times to augment each original image
+            lr_trans_range (array, optional): left-right translation choices. Defaults to np.arange(-10, 10, 1).
+            ap_trans_range (array, optional): anterior-posterior translation choices. Defaults to np.arange(-5, 15, 1).
+            si_trans_range (array, optional): superior-inferior translation choices. Defaults to np.arange(-3, 3, 1).
+            lr_rot_range (array, optional): rotation on left-right axis in degrees. Defaults to np.arange(-20, 20, 1).
+            ap_rot_range (array, optional): rotation on anterior-posterior axis in degrees. Defaults to np.arange(-20, 20, 1).
+            si_rot_range (array, optional): rotation on superior-inferior axis in degrees. Defaults to np.arange(-20, 20, 1).
+            scale_range (array, optional): relative isotropic scaling. Defaults to np.arange(0.9, 1.1, 0.05).
+            seed (int, optional): random seed. Defaults to None.
+        """        
         self.strOutputDir = os.path.abspath(output_dir)
         self.dfConfig = pd.DataFrame()
         nImages = len(input_files)
@@ -96,6 +116,11 @@ class AffineAugmenter:
             self.dfConfig.at[n, 'mat_path'] = strSavePath
 
     def run(self, n_pipeline_jobs=1):
+        """Run augmentations
+
+        Args:
+            n_pipeline_jobs (int, optional): Number of parallel processing jobs. Defaults to 1.
+        """        
         self._create_fsl_mats()
         if not os.path.exists(self.strOutputDir):
             os.makedirs(self.strOutputDir)
@@ -125,91 +150,19 @@ class AffineAugmenter:
         else:
             augment.run(plugin='MultiProc', plugin_args={'n_procs': n_pipeline_jobs})
 
-class AffineRegAugmenter:
-    def __init__(self, input_files, output_dir, n_augment):
-        self.strOutputDir = os.path.abspath(output_dir)
-        self.dfConfig = pd.DataFrame()
-        lsFunc = []
-        lsAnat = []
-        lsOutputFunc = []
-        lsOutputAnat = []
-        lsRefFunc = []
-        for tup in input_files:
-            lsFunc += [tup[0]] * n_augment
-            lsAnat += [tup[1]] * n_augment
-            lsOutputFunc += self._generate_output_name(tup[0], n_augment)
-            lsOutputAnat += self._generate_output_name(tup[1], n_augment)
-            lsRefFunc += list(np.random.choice(np.array(input_files)[:, 0], n_augment))
-        self.dfConfig['func'] = lsFunc
-        self.dfConfig['anat'] = lsAnat
-        self.dfConfig['output_func'] = lsOutputFunc
-        self.dfConfig['output_anat'] = lsOutputAnat
-        self.dfConfig['ref_func'] = lsRefFunc
-
-    def _generate_output_name(self, in_file, n_augment):
-        match = re.search(r'sub-([a-zA-Z0-9]+)_ses-([a-zA-Z0-9]+)_(.*)', in_file)
-        strSub = match.group(1)
-        strSes = match.group(2)
-        strSuffix = match.group(3)
-        strType = 'anat' if 'anat' in in_file else 'func'
-        for n in range(n_augment):
-            strDir = os.path.join(self.strOutputDir, 'sub-{sub}aug{n:02d}/ses-{ses}/{type}').format(sub=strSub, n=n,
-                                                                                                   ses=strSes,
-                                                                                                   type=strType)
-            if not os.path.exists(strDir):
-                os.makedirs(strDir)
-            strOutTemp = os.path.join(strDir, 'sub-{sub}aug{n:02d}_ses-{ses}_{suffix}').format(sub=strSub, n=n,
-                                                                                               ses=strSes, suffix=strSuffix)
-            yield strOutTemp
-
-    def run(self, n_pipeline_jobs=1):
-        if not os.path.exists(self.strOutputDir):
-            os.makedirs(self.strOutputDir)
-        strJobListPath = os.path.join(self.strOutputDir, 'joblist.csv')
-        self.dfConfig.to_csv(strJobListPath)
-
-        datanode = Node(utility.csv.CSVReader(in_file=os.path.abspath(strJobListPath), header=True),
-                        name='datanode')
-
-        augment = Workflow('augmentation_affinereg', base_dir=os.path.join(self.strOutputDir, 'working_dir'))
-
-        meanFunc = MapNode(fsl.MeanImage(), name='mean_func', iterfield=['in_file'])
-        augment.connect(datanode, 'func', meanFunc, 'in_file')
-        skullstripFunc = skullstrip.make_func_mask_workflow()
-        augment.connect(meanFunc, 'out_file', skullstripFunc, 'inputnode.mean_file')
-
-        meanRef = MapNode(fsl.MeanImage(), name='mean_ref', iterfield=['in_file'])
-        augment.connect(datanode, 'ref_func', meanRef, 'in_file')
-        skullstripRef = skullstrip.make_func_mask_workflow(name='refmask')
-        augment.connect(meanRef, 'out_file', skullstripRef, 'inputnode.mean_file')
-
-        regMask = MapNode(fsl.FLIRT(interp='nearestneighbour'), name='coregister_mask',
-                          iterfield=['in_file', 'reference'])
-        augment.connect(skullstripFunc, 'outputnode.mask', regMask, 'in_file')
-        augment.connect(skullstripRef, 'outputnode.mask', regMask, 'reference')
-
-        transformFunc = MapNode(fsl.ApplyXFM(interp='spline', apply_xfm=True), name='transform_func',
-                                iterfield=['in_file', 'reference', 'in_matrix_file', 'out_file'])
-        augment.connect(datanode, 'func', transformFunc, 'in_file')
-        augment.connect(datanode, 'func', transformFunc, 'reference')
-        augment.connect(regMask, 'out_matrix_file', transformFunc, 'in_matrix_file')
-        augment.connect(datanode, 'output_func', transformFunc, 'out_file')
-
-        transformAnat = MapNode(fsl.ApplyXFM(interp='spline', apply_xfm=True), name='transform_anat',
-                                iterfield=['in_file', 'reference', 'in_matrix_file', 'out_file'])
-        augment.connect(datanode, 'anat', transformAnat, 'in_file')
-        augment.connect(datanode, 'anat', transformAnat, 'reference')
-        augment.connect(regMask, 'out_matrix_file', transformAnat, 'in_matrix_file')
-        augment.connect(datanode, 'output_anat', transformAnat, 'out_file')
-
-        if n_pipeline_jobs == 1:
-            augment.run()
-        else:
-            augment.run(plugin='MultiProc', plugin_args={'n_procs': n_pipeline_jobs})
-
 
 class PredefinedAffineAugmenter:
     def __init__(self, input_func, input_anat, affines, output_func, output_anat, output_dir):
+        """Apply a predefined set of affine transformations to a list of images.
+
+        Args:
+            input_func (list): list of original fMRI images
+            input_anat (list): list of original structural MRI, corresponding to each original fMRI
+            affines (list): list of affine matrices to apply, in FSL .mat format
+            output_func (list): list of output paths for each transformed fMRI
+            output_anat (list): list of output paths for each transformed structural MRI
+            output_dir (str): path to store intermediate processing files
+        """        
         self.strOutputDir = os.path.abspath(output_dir)
         self.dfConfig = pd.DataFrame()
         self.dfConfig['func'] = input_func
@@ -219,6 +172,11 @@ class PredefinedAffineAugmenter:
         self.dfConfig['affine'] = affines
 
     def run(self, n_pipeline_jobs=1):
+        """Perform transformations.
+
+        Args:
+            n_pipeline_jobs (int, optional): number of parallel processing jobs. Defaults to 1.
+        """        
         if not os.path.exists(self.strOutputDir):
             os.makedirs(self.strOutputDir)
         strJobListPath = os.path.join(self.strOutputDir, 'joblist.csv')
